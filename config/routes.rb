7fdd2d8105f326500765d324ad3eba003782c651
TONGARARIN <tongararin@gmail.com>
2026-05-16 17:11:53 +0900

    ログイン機能とログアウト機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   root to: "books#index"
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
