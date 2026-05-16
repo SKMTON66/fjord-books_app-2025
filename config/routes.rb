5ed670a342e077969ff97e99274e62cbb2c30b55
TONGARARIN <tongararin@gmail.com>
2026-05-16 17:40:56 +0900

    パスワードを忘れた際、メール経由でパスワードを再設定できる機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root to: "books#index"
   devise_for :users
   resources :users
-  root to: "books#index"
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
