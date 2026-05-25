823a5c4dc2ae2a3e404b06083639d028408ad6a3
TONGARARIN <tongararin@gmail.com>
2026-05-25 15:19:27 +0900

    scaffoldで日報(report)のベースファイルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  devise_for :users
   root to: 'books#index'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  resources :reports
+  devise_for :users
   resources :books
   resources :users, only: %i(index show)
 
