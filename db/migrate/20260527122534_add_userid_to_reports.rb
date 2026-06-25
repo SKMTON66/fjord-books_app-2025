a261bcac9ab9aea0cae817452f5caa999fdace47
TONGARARIN <tongararin@gmail.com>
2026-06-25 16:49:27 +0900

    日報(reports)のmigrationファイルを1まとめにしました

--- db/migrate/20260527122534_add_userid_to_reports.rb
+++
@@ -1,5 +0,0 @@
-class AddUseridToReports < ActiveRecord::Migration[8.0]
-  def change
-    add_reference :reports, :user, null: true, foreign_key: true
-  end
-end
--- db/migrate/20260601042324_change_title_null_in_reports.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTitleNullInReports < ActiveRecord::Migration[8.0]
-  def change
-    change_column_null :reports, :title, false
-  end
-end
